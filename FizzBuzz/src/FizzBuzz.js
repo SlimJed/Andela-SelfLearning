cb707940c0cd39107565cba0bcf52069e775a9f1
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-08 21:18:15 +0100

    Refactored FizzBuzz | SOLVED

--- FizzBuzz/src/FizzBuzz.js
+++ FizzBuzz/src/FizzBuzz.js
@@ -1,9 +1,9 @@
 function fizzBuzz(num){
-	if (num % 15 == 0){
+	if (num % 15 === 0){
 		return "FizzBuzz";
-	}else if(num % 3 == 0){
+	}else if(num % 3 === 0){
 		return "Fizz";
-	}else if(num % 5 == 0){
+	}else if(num % 5 === 0){
 		return "Buzz";
 	}else {
 		return num;
