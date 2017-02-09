65bf3a35a557fc2ecacad1864cbb8ed3c982cadf
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-09 10:50:25 +0100

    Final Solution

--- aritGeo/src/aritGeo.js
+++ aritGeo/src/aritGeo.js
@@ -4,13 +4,15 @@ var myApp = {
 		if(arr.length == 0 || arr == undefined){
 			return 0;
 		}else{
-			var test1 = arr[1] - arr[0];
-			var test2 = arr[arr.length - 1] - arr[arr.length - 2];
-			var test3 = arr[1] / arr[0];
-			var test4 = arr[arr.length - 1] / arr[arr.length -2];
-			if(test1 == test2){
+			var apTest1 = arr[1] - arr[0];
+			var apTest2 = arr[arr.length - 1] - arr[arr.length - 2];
+			var apTest3 = arr[arr.length - 2] - arr[arr.length - 3];
+			var gpTest1 = arr[1] / arr[0];
+			var gpTest2 = arr[arr.length - 1] / arr[arr.length -2];
+			var gpTest3 = arr[arr.length - 2] / arr[arr.length -3];
+			if(apTest1 == apTest2 && apTest1 == apTest3){
 				return "Arithmetic";
-			}else if (test3 == test4){
+			}else if (gpTest1 == gpTest2 && gpTest1 == gpTest3){
 				return "Geometric";
 			}else{
 				return -1;
