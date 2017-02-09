11180bb980f4eddd45b07e470c53a6d3cdb0d511
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-09 11:24:23 +0100

    Added response for other datatypes other than Integers

--- FizzBuzz/src/FizzBuzz.js
+++ FizzBuzz/src/FizzBuzz.js
@@ -4,14 +4,18 @@ var myApp = {
 	
 	fizzBuzz: function (num){
 		var answer;
-		if (num % 15 === 0){
-			answer = "FizzBuzz";
-		}else if(num % 3 === 0){
-			answer = "Fizz";
-		}else if(num % 5 === 0){
-			answer = "Buzz";
-		}else {
-			answer = num;
+		if (typeof(num)  === 'number'){
+			if (num % 15 === 0){
+				answer = "FizzBuzz";
+			}else if(num % 3 === 0){
+				answer = "Fizz";
+			}else if(num % 5 === 0){
+				answer = "Buzz";
+			}else {
+				answer = num;
+			}
+		}else{
+			console.log("Input type must be an integer");
 		}
 		return answer;
 	}
